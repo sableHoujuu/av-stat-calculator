7658eeca00762a34ecfd58ab8f8e91e757b47bfa
sableHoujuu <dktpoole3@gmail.com>
2026-07-01 12:59:31 -0400

    created first data loader function and implemented it in main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sys
+
+import numpy as np
+import pandas as pd
+
+from src.data_loader import get_unit_data
+
+
 def main():
-    print("Hello from av-stat-calculator!")
+    unit_name = input("Enter unit name: ")
+    print("Finding unit...")
+    matching_units = get_unit_data(unit_name.lower())
+    if matching_units is pd.DataFrame:
+        print("More than 1 unit found, implement this later")
+        sys.exit(1)
+    unit_level = input("Unit found.\nEnter unit level: ")
 
 
 if __name__ == "__main__":
---
+++ src/data_loader.py
@@ -0,0 +1,19 @@
+import sys
+
+import numpy as np
+import pandas as pd
+
+# Initializing the data once at runtime, we don't want to do this every time it's called
+unit_data = pd.read_csv("data/units.csv", index_col="unit_id")
+
+
+def get_unit_data(name: str) -> pd.Series | pd.DataFrame:
+    try:
+        unit_rows = unit_data[unit_data["unit_name".lower()] == name]
+    except KeyError:
+        print("Can't find specified unit, exiting.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(1)
+    return unit_rows
